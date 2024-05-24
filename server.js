0082591ff50e90c2afd81f953be3e94ca855920c
Omkar Patil <ommipatil13@gmail.com>
2024-05-24 14:12:06 +0530

    new

---
+++ server.js
@@ -0,0 +1,42 @@
+// const express = require("express");
+import express from 'express';
+import dotenv from "dotenv";
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoute from './routes/authRoute.js';
+import categoryRoute from './routes/categoryRoutes.js';
+import productRoute from './routes/productRoutes.js';
+import cors from 'cors';
+
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+//routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoute);
+
+//rest api
+app.get('/', (req, res) => {
+    res.send("<h1> Welcome to E-Comm MERN Stack Web App </h1>");
+});
+
+//PORT
+const PORT = process.env.PORT;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
+});
